11d2a16da98dbddb8cc6b381abd3670f2e4e6d68
JacobF94 <jacob.frensley@gmail.com>
2021-12-07 14:36:38 -0600

    final project

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,9 +1,40 @@
 import { getOrders } from "./database.js"
+import { getMetals } from "./database.js"
+import { getSizes } from "./database.js"
+import { getStyles } from "./database.js"
 
 const buildOrderListItem = (order) => {
-    return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
-    </li>`
+    const metals = getMetals()
+    const sizes = getSizes()
+    const styles = getStyles()
+
+    const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+const foundSize = sizes.find(
+    (size) => {
+        return size.id === order.sizeId
+    }
+)
+
+const foundStyle = styles.find(
+    (style) => {
+        return style.id === order.styleId
+    }
+)
+
+const totalCost = foundMetal.price + foundStyle.price + foundSize.price
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+
+return `<li>
+    Order #${order.id} cost ${costString}
+</li>`
 }
 
 export const Orders = () => {
@@ -22,4 +53,3 @@ export const Orders = () => {
 
     return html
 }
-
